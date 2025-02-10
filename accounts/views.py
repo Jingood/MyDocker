ee5af2b878210123a975f83ad81d7ed41d6ee920
박진수 <pajinsu0803@naver.com>
2025-02-10 22:55:31 +0900

    accounts/views.py 회원가입 로직 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import get_user_model
+from .serializers import SignupSerializer
+from .models import User
 
-# Create your views here.
+class SignupAPIView(APIView):
+    permission_classes = [AllowAny]
+    
+    def post(self, request):
+        serializers = SignupSerializer(data=request.data)
+        if serializers.is_valid(raise_exception=True):
+            serializers.save()
+        return Response(serializers.data, status=status.HTTP_200_OK)
+
+
+
+
+
+
+
