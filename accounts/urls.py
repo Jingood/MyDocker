7ae46bcc5fd6c1865edb9bf2a6850fead93ca396
박진수 <pajinsu0803@naver.com>
2025-02-10 23:16:37 +0900

    CustomTokenObtainPairView 생성 로그인 시 access token, refresh token 서버 세션에 저장

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+from accounts import views
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenBlacklistView
 )
-from . import views
+
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('api/signup/', views.SignupAPIView.as_view(), name='signup'),
+    path('api/login/', views.CustomTokenObtainPairView.as_view(), name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import get_user_model
 from .serializers import SignupSerializer
@@ -16,9 +17,15 @@ class SignupAPIView(APIView):
             serializers.save()
         return Response(serializers.data, status=status.HTTP_200_OK)
 
+class CustomTokenObtainPairView(TokenObtainPairView):
 
-
-
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        tokens = response.data
+        request.session['access'] = tokens.get('access')
+        request.session['refresh'] = tokens.get('refresh')
+        request.session.save()
+        return response
 
 
 
