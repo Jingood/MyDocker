885efb7e70888f6e2c9ff3ed7b513f2216a11715
박진수 <pajinsu0803@naver.com>
2025-02-25 00:15:41 +0900

    1. login 로직 서버 세션 저장에서 쿠키 저장으로 변경 2. logout 로직 생성

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import get_user_model
 from .serializers import SignupSerializer
@@ -20,12 +21,54 @@ class SignupAPIView(APIView):
 class CustomTokenObtainPairView(TokenObtainPairView):
 
     def post(self, request, *args, **kwargs):
-        response = super().post(request, *args, **kwargs)
-        tokens = response.data
-        request.session['access'] = tokens.get('access')
-        request.session['refresh'] = tokens.get('refresh')
-        request.session.save()
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        tokens = serializer.validated_data
+        user_id = serializer.user.id
+
+        response = Response(tokens, status=status.HTTP_200_OK)
+
+        response.set_cookie(
+            key='access_token',
+            value=tokens.get('access'),
+            httponly=True,
+            secure=False,
+            samesite='Lax'
+        )
+
+        response.set_cookie(
+            key='refresh_token',
+            value=tokens.get('refresh'),
+            httponly=True,
+            secure=False,
+            samesite='Lax'
+        )
+
+        response.set_cookie(
+            key='user_id',
+            value=user_id,
+            httponly=False,
+            secure=False,
+            samesite='Lax'
+        )
         return response
 
+class LogoutAPIView(APIView):
+    permission_classes = [IsAuthenticated]
 
-
+    def post(self, request):
+        refresh_token = request.COOKIES.get('refresh_token')
+        if not refresh_token:
+            return Response({"error":"Refresh Token Not Found."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+        except Exception as e:
+            return Response({"error": f"Token Blacklisting failed: {str(e)}"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        response = Response(status=status.HTTP_200_OK)
+        response.delete_cookie('access_token')
+        response.delete_cookie('refresh_token')
+        response.delete_cookie('user_id')
+        return response
