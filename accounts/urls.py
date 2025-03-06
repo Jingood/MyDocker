cac0ec2b467fb86cce23ecf50f093a2c04fdb132
박진수 <pajinsu0803@naver.com>
2025-03-06 17:38:21 +0900

    accounts/urls.py 작성 ProfileAPIView와 연결

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     path('api/signup/', views.SignupAPIView.as_view(), name='signup'),
     path('api/login/', views.CustomTokenObtainPairView.as_view(), name='login'),
     path('api/logout/', views.LogoutAPIView.as_view(), name='logout'),
+    path('api/profile/change_password/', views.ChangePasswordAPIView.as_view(), name="change_pw"),
     path('api/delete/', views.DeleteUserAPIView.as_view(), name='delete'),
+    path('api/profile/', views.ProfileAPIView.as_view(), name="profile"),
 ]
