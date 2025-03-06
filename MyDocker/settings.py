8b0dc6919b12b2465ae7babbe055ae4b10adae24
박진수 <pajinsu0803@naver.com>
2025-03-06 17:57:52 +0900

    settings.py 수정 REST_FRAMEWORK 설정 추가

--- MyDocker/settings.py
+++ MyDocker/settings.py
@@ -110,6 +110,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES' : {
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    },
+}
+
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
