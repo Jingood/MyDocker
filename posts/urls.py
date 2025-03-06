dc204427e8722f1e98624a8e5d181d5ab839e3ff
박진수 <pajinsu0803@naver.com>
2025-03-06 22:19:13 +0900

    posts/urls.py 수정 views.py와 인자 맞춤

--- posts/urls.py
+++ posts/urls.py
@@ -4,9 +4,9 @@ from posts import views
 urlpatterns = [
     path("api/", views.PostListCreateAPIView.as_view(), name='post_cr'),
     path("api/<int:pk>/", views.PostDetailAPIView.as_view(), name="post_ud"),
-    path("api/like/<int:pk>/", views.PostLikeAPIView.as_view(), name="post_like"),
+    path("api/like/<int:post_id>/", views.PostLikeAPIView.as_view(), name="post_like"),
     path("api/comment/<int:post_id>/", views.CommentCreateAPIView.as_view(), name="comment_c"),
     path("api/comment/detail/<int:comment_id>/", views.CommentDetailAPIView.as_view(), name="comment_ud"),
-    path("api/comment/reply/<int:post_id>/<int:comment_id>/", views.ReplyCreateAPIView.as_view(), name="reply_c"),
+    path("api/comment/reply/<int:post_id>/<int:parent_id>/", views.ReplyCreateAPIView.as_view(), name="reply_c"),
     path("api/comment/like/<int:comment_id>/", views.CommentLikeAPIView.as_view(), name="comment_like"),
 ]
