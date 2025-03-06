3733431e9042d1af17772445d26f500e40c0f003
박진수 <pajinsu0803@naver.com>
2025-03-06 17:34:53 +0900

    accounts/serializers.py 오타 수정 SignupSerializer 수정

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 class SignupSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        field = [
+        fields = [
             "username",
             "password"
         ]
