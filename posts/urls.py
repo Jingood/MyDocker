3308bf2745b7dc690e5e67b4c5698878d3512527
박진수 <pajinsu0803@naver.com>
2025-03-06 22:22:28 +0900

    urls.py 재수정

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,7 @@ from posts import views
 
 urlpatterns = [
     path("api/", views.PostListCreateAPIView.as_view(), name='post_cr'),
-    path("api/<int:pk>/", views.PostDetailAPIView.as_view(), name="post_ud"),
+    path("api/<int:post_id>/", views.PostDetailAPIView.as_view(), name="post_ud"),
     path("api/like/<int:post_id>/", views.PostLikeAPIView.as_view(), name="post_like"),
     path("api/comment/<int:post_id>/", views.CommentCreateAPIView.as_view(), name="comment_c"),
     path("api/comment/detail/<int:comment_id>/", views.CommentDetailAPIView.as_view(), name="comment_ud"),
